8654ea0ab46fe13f6266be3c1b103c77d318bc79
Pavithran <pavithrank02@gmail.com>
2022-10-08 21:15:27 +0530

    useEffect usage

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 
 import MoviesList from './components/MoviesList';
 import './App.css';
@@ -7,7 +7,9 @@ function App() {
   const [movies, setMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
- async function fetchMoviesHandler() {
+
+
+ const fetchMoviesHandler = useCallback(async () => {
    setIsLoading(true);
    setError(null);
    try {
@@ -32,7 +34,11 @@ function App() {
       }
       setIsLoading(false);
     
-  }
+  }, []);
+  useEffect(() => {
+    fetchMoviesHandler();
+  }, [fetchMoviesHandler]);
+
   let content = <p>Found No Movies.</p>
   if(movies.length > 0) {
     content = <MoviesList movies={movies} />
