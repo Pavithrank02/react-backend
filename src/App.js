709884fd994b9e16e6f67bc116d30f090444474e
Pavithran <pavithrank02@gmail.com>
2022-10-08 20:50:34 +0530

    error error handling

--- src/App.js
+++ src/App.js
@@ -5,11 +5,18 @@ import './App.css';
 
 function App() {
   const [movies, setMovies] = useState([]);
-
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
  async function fetchMoviesHandler() {
-    const response = await fetch('https://swapi.dev/api/films/')
-      
-        const data = await response.json();
+   setIsLoading(true);
+   setError(null);
+   try {
+    const response = await fetch('https://swapi.dev/api/films/');
+    
+    if(!response.ok) {
+      throw new Error("Something went wrong")
+    }
+    const data = await response.json();
         const transformedMovies = data.results.map((movieData) => {
           return {
             id: movieData.episode_id,
@@ -19,6 +26,22 @@ function App() {
           };
         });
         setMovies(transformedMovies);
+        
+      }catch (error) {
+        setError(error.message);
+      }
+      setIsLoading(false);
+    
+  }
+  let content = <p>Found No Movies.</p>
+  if(movies.length > 0) {
+    content = <MoviesList movies={movies} />
+  }
+  if(error) {
+    content = <p>{error}</p>
+  }
+  if(isLoading) {
+    content = <p>Loading...</p>
   }
 
   return (
@@ -27,7 +50,7 @@ function App() {
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={movies} />
+       {content}
       </section>
     </React.Fragment>
   );
